0011e1aea0bc78937087c1e869f102648020ca4d
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-12-16 13:04:56 +0545

    create job model

---
+++ prisma/migrations/20241216071603_create_job_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Job` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `company` VARCHAR(191) NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Job` ADD CONSTRAINT `Job_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
